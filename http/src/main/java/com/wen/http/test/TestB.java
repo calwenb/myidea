81d5407ed842bc65a832c994f1552d0a11a04c7d
calvinwen <5768t8Gj>
2022-08-04 16:29:43 +0800

    增加 http 测试模块。

---
+++ http/src/main/java/com/wen/http/test/TestB.java
@@ -0,0 +1,15 @@
+package com.wen.http.test;
+
+import com.alibaba.fastjson2.JSON;
+
+/**
+ * @author calwen
+ * @eamil calvinwen@dianhun.cn
+ * @since 2022/8/4
+ */
+public class TestB {
+    public static void main(String[] args) {
+//        String js="{\"parentId\": 0}";
+//        System.out.println(o);
+    }
+}
